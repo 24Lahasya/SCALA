c232d42fcc437866bb43bb01d7f8af3e0dd12f81
Lahasya <lahasya24@gmail.com>
2023-04-22 08:45:02 +0530

    Partial Function In scala

---
+++ src/main/scala/PartialFunction.scala
@@ -0,0 +1,16 @@
+object PartialFunction {
+  def main(args: Array[String]): Unit = {
+    def getBill(vatPercentage: Int, serviceTax: Int, netBill: Double): Double = {
+      netBill * (vatPercentage / 100.0) + serviceTax
+    }
+    def calculateBill: Double => Double = getBill(20,100,_:Double)
+    val abill = getBill(20, 100, 400)
+    println(abill)
+
+    val actbill = calculateBill(400)
+    println(s"===>$actbill")
+
+
+
+  }
+}
