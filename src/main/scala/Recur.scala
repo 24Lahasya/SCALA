619377652d529f6b2813dada9cf28bf75660c81a
Lahasya <lahasya24@gmail.com>
2023-04-22 08:46:22 +0530

    Recursive In scala

---
+++ src/main/scala/Recur.scala
@@ -0,0 +1,16 @@
+object Recur {
+  def main(args: Array[String]): Unit = {
+    def factorial(n:BigInt):BigInt={
+      var x=65
+      var y=56
+      var x1=52
+      var x2=98
+      var x3=89
+      if(n<=1)
+        1
+      else
+        n * factorial(n-1)
+
+    }
+  }
+}
